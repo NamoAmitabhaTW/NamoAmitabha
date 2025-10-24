4519b053cf953ae481eb05952c3d610d04407edb
Namoamitabha <namoamitabha1995@gmail.com>
2025-10-24 10:19:25 +0800

    build(gradle): set minSdk=29 to align with Meta Horizon requirements

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -25,7 +25,7 @@ android {
         applicationId = "com.earth.amitabha"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 23
+        minSdk = 29
         targetSdk = 35
         versionCode = flutter.versionCode
         versionName = flutter.versionName
