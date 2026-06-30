78e7dc60e23127ec71470c8a837225d7cff1a40d
Namoamitabha <namoamitabha1995@gmail.com>
2026-07-01 04:20:16 +0800

    feat: localize app name and microphone permission for all locales

--- android/app/flavorizr.gradle.kts
+++ android/app/flavorizr.gradle.kts
@@ -9,25 +9,21 @@ android.apply {
         create("dev") {
             dimension = "app"
             applicationId = "com.earth.amitabha.dev"
-            resValue(type = "string", name = "app_name", value = "念佛 Dev")
         }
         create("staging") {
             dimension = "app"
             applicationId = "com.earth.amitabha.staging"
-            resValue(type = "string", name = "app_name", value = "念佛 Staging")
         }
         create("prod") {
             dimension = "app"
             minSdk = 23
             applicationId = "com.earth.amitabha"
-            resValue(type = "string", name = "app_name", value = "念佛")
         }
         create("prodMeta") {
             dimension = "app"
             applicationId = "com.earth.amitabha"
             minSdk = 29
             targetSdk = 32    
-            resValue(type = "string", name = "app_name", value = "念佛")
         }   
     }
 }
